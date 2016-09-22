ff9bf29c95caeab0609a044c1174f8648e61e37d
IncidentNormal <admin@incidentnormal.uk>
2016-09-22 20:17:02 +0100

    Deploy to GitHub Pages @ (Thu 22 Sep 20:17:02 BST 2016)

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,24 @@ set -e # halt script on error
 bundle exec jekyll build
 # bundle exec travis-lint
 # bundle exec htmlproof ${HTML_FOLDER} --disable-external
+HTML_FOLDER=$(pwd)
 
-cd ${HTML_FOLDER}
+echo "\${HTML_FOLDER}: [${HTML_FOLDER}]"
+
+if [ -n ${HTML_FOLDER} ] && [[ ${HTML_FOLDER} =~ ^(/[^/ ]*)+/?$ ]]; then
+    cd ${HTML_FOLDER}
+else
+    echo "\${HTML_FOLDER} not set. Exiting."
+    exit 1
+fi
 
 # config
-git config --global user.email "nandomoreira.me@gmail.com"
-git config --global user.name "Fernando Moreira"
+git config --global user.email "admin@incidentnormal.uk"
+git config --global user.name "IncidentNormal"
 
 # deploy
 git init
 git add --all
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:gh-pages
+git commit -m "Deploy to GitHub Pages @ ($(date))"
+#git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:gh-pages
+git push origin master
